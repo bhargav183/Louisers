eaea694270f5d356a188395325afd0845e23a443
bhargav183 <brr0392@gmail.com>
2019-02-21 21:59:12 +0530

    Querying

--- stock.js
+++ stock.js
@@ -1,12 +1,14 @@
-var ward = document.getElementById("ward");
+var ward = document.getElementById("ward").value;
 var item = document.getElementById("item");
-var Ref = firebase.database().ref("ward/");
+var Ref = firebase.database().ref("Ward/Hebbal");
 
 function submitClick(){
 Ref.orderByValue().on("value", function(data) {
-   
+   document.write("<th>" + "Object       " + "</th>" + "<th>" + "	Value" + "</th> <br/>");
    data.forEach(function(data) {
-      console.log("The " + data.key + " rating is " + data.val());
+      console.log(data.key + ": " + data.val());
+	  
+	  document.write("<tr>" + data.key + "	</tr>" + "<tr>" + data.val() + "	</tr><br/>");
    });
 });
 }
