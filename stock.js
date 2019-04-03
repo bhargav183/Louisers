82acd1bc5f6ca3ce4c1099ce501bcf4480d9c3b5
bhargav183 <brr0392@gmail.com>
2019-04-03 07:41:56 +0530

    Changes in Data Analysis

--- stock.js
+++ stock.js
@@ -1,14 +1,14 @@
 var ward = document.getElementById("ward").value;
-var item = document.getElementById("item");
-var Ref = firebase.database().ref("Ward/Hebbal");
+var Ref = firebase.database().ref("Ward/"+ward);
 
 function submitClick(){
 Ref.orderByValue().on("value", function(data) {
+   alert("Hello");
    document.write("<th>" + "Object       " + "</th>" + "<th>" + "	Value" + "</th> <br/>");
    data.forEach(function(data) {
-      console.log(data.key + ": " + data.val());
-	  
+      console.log(data.key + ": " + data.val());	  
 	  document.write("<tr>" + data.key + "	</tr>" + "<tr>" + data.val() + "	</tr><br/>");
+	  alert("hello");
    });
 });
 }
